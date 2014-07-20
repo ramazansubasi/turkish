32b08adfcdd6115b1fceb158732e8ed5c1805cb7
gevv <320voltmail@gmail.com>
2014-07-20 15:33:41 +0300

    Update Modlog.turkish.php

--- Modlog.turkish.php
+++ Modlog.turkish.php
@@ -1,79 +1,79 @@
 <?php
 // Version: 2.0; Modlog
 
-$txt['modlog_date'] = 'Date';
-$txt['modlog_member'] = 'Member';
-$txt['modlog_position'] = 'Position';
-$txt['modlog_action'] = 'Action';
+$txt['modlog_date'] = 'Tarih';
+$txt['modlog_member'] = 'Üye';
+$txt['modlog_position'] = 'Üye Grubu';
+$txt['modlog_action'] = 'Eylem';
 $txt['modlog_ip'] = 'IP';
 $txt['logged'] = 'Logged';
-$txt['modlog_search_result'] = 'Search Results';
-$txt['modlog_total_entries'] = 'Total Entries';
-$txt['modlog_ac_approve_topic'] = 'Approved topic &quot;{topic}&quot; by &quot;{member}&quot;';
-$txt['modlog_ac_approve'] = 'Approved message &quot;{subject}&quot; in &quot;{topic}&quot; by &quot;{member}&quot;';
-$txt['modlog_ac_lock'] = 'Locked &quot;{topic}&quot;';
-$txt['modlog_ac_warning'] = 'Warned {member} for &quot;{message}&quot;';
-$txt['modlog_ac_unlock'] = 'Unlocked &quot;{topic}&quot;';
-$txt['modlog_ac_pin'] = 'Pinned &quot;{topic}&quot;';
-$txt['modlog_ac_unpin'] = 'Unpinned &quot;{topic}&quot;';
-$txt['modlog_ac_delete'] = 'Deleted &quot;{subject}&quot; by &quot;{member}&quot; from &quot;{topic}&quot;';
-$txt['modlog_ac_delete_member'] = 'Deleted member &quot;{name}&quot;';
+$txt['modlog_search_result'] = 'Arama Sonuçları';
+$txt['modlog_total_entries'] = 'Toplam Girdi';
+$txt['modlog_ac_approve_topic'] = '&quot;{topic}&quot; konusu &quot;{member}&quot; tarafından onaylanmıştır.';
+$txt['modlog_ac_approve'] = '&quot;{topic}&quot; konusundaki &quot;{subject}&quot; iletisi &quot;{member}&quot; tarafından onaylanmıştır';
+$txt['modlog_ac_lock'] = 'Kilitleme: &quot;{topic}&quot;';
+$txt['modlog_ac_warning'] = '&quot;{message}&quot; için {member} uyarılmıştır';
+$txt['modlog_ac_unlock'] = 'Kilidi kaldırıldı: &quot;{topic}&quot;';
+$txt['modlog_ac_pin'] = 'Sabitleme: &quot;{topic}&quot;';
+$txt['modlog_ac_unpin'] = 'Sabitliği kaldırıldı: &quot;{topic}&quot; ';
+$txt['modlog_ac_delete'] = 'Silme: &quot;{subject}&quot; / &quot;{topic}&quot; - &quot;{member}&quot;';
+$txt['modlog_ac_delete_member'] = 'Üye Silme: &quot;{name}&quot;';
 $txt['modlog_ac_merge_member'] = 'Merged member &quot;{name}&quot; with &quot;{member}&quot;';
-$txt['modlog_ac_remove'] = 'Removed topic &quot;{topic}&quot; from &quot;{board}&quot;';
-$txt['modlog_ac_modify'] = 'Edited &quot;{message}&quot; by &quot;{member}&quot;';
-$txt['modlog_ac_merge'] = 'Merged topics to create &quot;{topic}&quot;';
-$txt['modlog_ac_split'] = 'Split &quot;{topic}&quot; to create &quot;{new_topic}&quot;';
-$txt['modlog_ac_move'] = 'Moved &quot;{topic}&quot; from &quot;{board_from}&quot; to &quot;{board_to}&quot;';
-$txt['modlog_ac_profile'] = 'Edit the profile of &quot;{member}&quot;';
-$txt['modlog_ac_pruned'] = 'Pruned some posts older than {days} days';
-$txt['modlog_ac_news'] = 'Edited the news';
-$txt['modlog_enter_comment'] = 'Enter Moderation Comment';
-$txt['modlog_moderation_log'] = 'Moderation Log';
-$txt['modlog_moderation_log_desc'] = 'Below is a list of all the moderation actions that have been carried out by moderators of the forum.<br><strong>Please note:</strong> Entries cannot be removed from this log until they are at least twenty-four hours old.';
-$txt['modlog_no_entries_found'] = 'There are currently no moderation log entries.';
-$txt['modlog_removeall'] = 'Remove All';
-$txt['modlog_go'] = 'Go';
-$txt['modlog_add'] = 'Add';
-$txt['modlog_search'] = 'Quick Search';
-$txt['modlog_by'] = 'By';
-$txt['modlog_id'] = '<em>Deleted - ID:%1$d</em>';
+$txt['modlog_ac_remove'] = 'Konu Kaldırma: &quot;{topic}&quot; / &quot;{board}&quot;';
+$txt['modlog_ac_modify'] = 'Düzenleme: &quot;{message}&quot; - &quot;{member}&quot;';
+$txt['modlog_ac_merge'] = 'Konu Birleştirm: &quot;{topic}&quot;';
+$txt['modlog_ac_split'] = 'Konu Parçalama &quot;{topic}&quot; (yeni konu: &quot;{new_topic}&quot;)';
+$txt['modlog_ac_move'] = 'Taşıma: &quot;{topic}&quot; / &quot;{board_from}&quot; --> &quot;{board_to}&quot;';
+$txt['modlog_ac_profile'] = 'Profil Düzenleme: &quot;{member}&quot;';
+$txt['modlog_ac_pruned'] = 'Konu Temizleme: {days} günlük';
+$txt['modlog_ac_news'] = 'Haberleri Düzenleme';
+$txt['modlog_enter_comment'] = 'Moderasyon Yorumu Girme';
+$txt['modlog_moderation_log'] = 'Moderasyon Kaydı';
+$txt['modlog_moderation_log_desc'] = 'Forum moderatörleri tarafından gerçekleştirilmiş eylemlerinin tümünün listesi aşağıdadır.<br /><strong>Not:</strong> Bu kayıttaki girdiler, girdi saatinden ancak 24 saat sonra silinebilirler.';
+$txt['modlog_no_entries_found'] = 'Girdi bulunamadı.';
+$txt['modlog_removeall'] = 'Tümünü Kaldır';
+$txt['modlog_go'] = 'Git';
+$txt['modlog_add'] = 'Ekle';
+$txt['modlog_search'] = 'Hızlı Arama';
+$txt['modlog_by'] = '-';
+$txt['modlog_id'] = '<em>Silindi - ID:%1$d</em>';
 
-$txt['modlog_ac_ban'] = 'Added ban triggers:';
-$txt['modlog_ac_ban_trigger_member'] = ' <em>Member:</em> {member}';
-$txt['modlog_ac_ban_trigger_email'] = ' <em>Email:</em> {email}';
-$txt['modlog_ac_ban_trigger_ip_range'] = ' <em>IP:</em> {ip_range}';
-$txt['modlog_ac_ban_trigger_hostname'] = ' <em>Hostname:</em> {hostname}';
+$txt['modlog_ac_ban'] = 'Üye yasakladı:';
+$txt['modlog_ac_ban_trigger_member'] = ' <em>Kullanıcı Adı:</em> {member}';
+$txt['modlog_ac_ban_trigger_email'] = ' <em>E-Posta:</em> {email}';
+$txt['modlog_ac_ban_trigger_ip_range'] = '<em>IP:</em> {ip_range}';
+$txt['modlog_ac_ban_trigger_hostname'] = ' <em>Alanadı:</em> {hostname}';
 
-$txt['modlog_admin_log'] = 'Administration Log';
-$txt['modlog_admin_log_desc'] = 'Below is a list of administration actions which have been logged on your forum.<br><strong>Please note:</strong> Entries cannot be removed from this log until they are at least twenty-four hours old.';
-$txt['modlog_admin_log_no_entries_found'] = 'There are currently no administration log entries.';
+$txt['modlog_admin_log'] = 'Yönetici Kaydı';
+$txt['modlog_admin_log_desc'] = 'Forumdaki yönetimsel eylemlerin tümünün listesi aşağıdadır.<br /><strong>Not:</strong> Bu kayıttaki girdiler, girdi saatinden ancak 24 saat sonra silinebilirler.';
+$txt['modlog_admin_log_no_entries_found'] = 'Şu anda admin kayıt geçmişi bulunmamaktadır.';
 
 // Admin type strings.
-$txt['modlog_ac_upgrade'] = 'Upgraded the forum to version {version}';
-$txt['modlog_ac_install'] = 'Installed version {version}';
-$txt['modlog_ac_add_board'] = 'Added a new board: &quot;{board}&quot;';
-$txt['modlog_ac_edit_board'] = 'Edited the &quot;{board}&quot; board';
-$txt['modlog_ac_delete_board'] = 'Deleted the &quot;{boardname}&quot; board';
-$txt['modlog_ac_add_cat'] = 'Added a new category, &quot;{catname}&quot;';
-$txt['modlog_ac_edit_cat'] = 'Edited the &quot;{catname}&quot; category';
-$txt['modlog_ac_delete_cat'] = 'Deleted the &quot;{catname}&quot; category';
+$txt['modlog_ac_upgrade'] = 'Forum Güncelleme: {version}';
+$txt['modlog_ac_install'] = 'Forum Yükleme: {version}';
+$txt['modlog_ac_add_board'] = 'Yeni Bölüm Ekleme: &quot;{board}&quot;';
+$txt['modlog_ac_edit_board'] = 'Bölüm Düzenleme: &quot;{board}&quot;';
+$txt['modlog_ac_delete_board'] = 'Bölüm Kaldırma: &quot;{boardname}&quot;';
+$txt['modlog_ac_add_cat'] = 'Kategori Ekleme: &quot;{catname}&quot;';
+$txt['modlog_ac_edit_cat'] = 'Kategori Düzenleme: &quot;{catname}&quot;';
+$txt['modlog_ac_delete_cat'] = 'Kategori Kaldırma: &quot;{catname}&quot;';
 
-$txt['modlog_ac_delete_group'] = 'Deleted the &quot;{group}&quot; group';
-$txt['modlog_ac_add_group'] = 'Added the &quot;{group}&quot; group';
-$txt['modlog_ac_edited_group'] = 'Edited the &quot;{group}&quot; group';
-$txt['modlog_ac_added_to_group'] = 'Added &quot;{member}&quot; to the &quot;{group}&quot; group';
-$txt['modlog_ac_removed_from_group'] = 'Removed &quot;{member}&quot; from the &quot;{group}&quot; group';
-$txt['modlog_ac_removed_all_groups'] = 'Removed &quot;{member}&quot; from all groups';
+$txt['modlog_ac_delete_group'] = 'Grup Kaldırma: &quot;{group}&quot;';
+$txt['modlog_ac_add_group'] = 'Grup Ekleme &quot;{group}&quot;';
+$txt['modlog_ac_edited_group'] = 'Grup Düzenleme: &quot;{group}&quot;';
+$txt['modlog_ac_added_to_group'] = 'Üye Grubu Değiştirme: &quot;{member}&quot; --> &quot;{group}&quot;';
+$txt['modlog_ac_removed_from_group'] = 'Üye Grubundan Çıkarma: &quot;{member}&quot; - &quot;{group}&quot;';
+$txt['modlog_ac_removed_all_groups'] = 'Tüm Üye Gruplarından Çıkarılma: &quot;{member}&quot;';
 
-$txt['modlog_ac_remind_member'] = 'Sent out a reminder to &quot;{member}&quot; to activate their account';
-$txt['modlog_ac_approve_member'] = 'Approved/Activated the account of &quot;{member}&quot;';
-$txt['modlog_ac_newsletter'] = 'Sent Newsletter';
+$txt['modlog_ac_remind_member'] = 'Aktivasyon Hatırlatması: &quot;{member}&quot;';
+$txt['modlog_ac_approve_member'] = 'Üyelik Aktivasyonu: &quot;{member}&quot;';
+$txt['modlog_ac_newsletter'] = 'Gönderilmiş Haber Bülteni';
 
-$txt['modlog_ac_enable_plugin'] = 'Enabled plugin: &quot;{plugin}&quot;, version {version}';
-$txt['modlog_ac_disable_plugin'] = 'Disabled plugin: &quot;{plugin}&quot;, version {version}';
+$txt['modlog_ac_enable_plugin'] = 'Paket etkinleştirildi: &quot;{plugin}&quot;, versiyon {version}';
+$txt['modlog_ac_disable_plugin'] = 'Paket devredışı bırakıldı: &quot;{plugin}&quot;, versiyon {version}';
 
 // Restore topic.
-$txt['modlog_ac_restore_topic'] = 'Restored topic &quot;{topic}&quot; from &quot;{board}&quot; to &quot;{board_to}&quot;';
-$txt['modlog_ac_restore_posts'] = 'Restored posts from &quot;{subject}&quot; to the topic &quot;{topic}&quot; in the &quot;{board}&quot; board.';
+$txt['modlog_ac_restore_topic'] = 'Konu Geri Yükleme: &quot;{topic}&quot; / &quot;{board}&quot; --> &quot;{board_to}&quot;';
+$txt['modlog_ac_restore_posts'] = 'İleti Geri Yükleme &quot;{subject}&quot; / &quot;{topic}&quot; / &quot;{board}&quot;';
 
-$txt['modlog_parameter_guest'] = '<em>Guest</em>';
+$txt['modlog_parameter_guest'] = '<em>Ziyaretçi</em>';
