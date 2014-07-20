925bb3d6c59fe0ca2104b5d0551cc69a2a141b4a
gevv <320voltmail@gmail.com>
2014-07-20 15:13:24 +0300

    Update Themes.turkish.php

--- Themes.turkish.php
+++ Themes.turkish.php
@@ -1,32 +1,32 @@
 <?php
 // Version: 2.0; Themes
 
-$txt['themeadmin_explain'] = 'Themes are the different looks and feels of your forum. These settings affect the selection of themes, and which themes guests and other members use.';
+$txt['themeadmin_explain'] = 'Temalar, forum\'unuzun görüntüsünü değiştiren paketlerdir. Bu ayarlar, tema seçimini ve ziyaretçiler ile üyelerin kullanacakları temaları belirler. ';
 
-$txt['theme_allow'] = 'Allow members to select their own skins?';
-$txt['theme_guests'] = 'Overall forum default for guests';
-$txt['theme_guests_mobile'] = 'Overall forum default for guests (mobile mode)';
-$txt['theme_select'] = 'choose...';
-$txt['theme_reset'] = 'Reset everyone to';
-$txt['theme_nochange'] = 'No change';
-$txt['theme_forum_default'] = 'Forum Default';
+$txt['theme_allow'] = 'Üyelerin kullanacakları temayı kendilerinin seçmelerine izin ver?';
+$txt['theme_guests'] = 'Tüm kullanıcılar, ziyaretciler için forum varsayılanı';
+$txt['theme_guests_mobile'] = 'Tüm kullanıcılar, ziyaretciler için forum varsayılanı (mobil)';
+$txt['theme_select'] = 'seç...';
+$txt['theme_reset'] = 'Herkesi sıfırla';
+$txt['theme_nochange'] = 'Değişiklik yok';
+$txt['theme_forum_default'] = 'Forum Varsayılanı';
 
 $txt['theme_remove_confirm'] = 'Are you sure you want to permanently remove this theme?';
 
-$txt['theme_install'] = 'Install a New Theme';
-$txt['theme_install_file'] = 'From a file';
-$txt['theme_install_dir'] = 'From a directory on the server';
-$txt['theme_install_error'] = 'That theme directory doesn\'t exist, or doesn\'t contain a theme!';
-$txt['theme_install_write_error'] = 'The Themes directory must be writable to continue!';
-$txt['theme_install_go'] = 'Install!';
-$txt['theme_install_new'] = 'Or create a copy of the default theme, named';
-$txt['theme_install_new_confirm'] = 'Are you sure you want to install this new theme?';
-$txt['theme_install_writable'] = 'Warning - you cannot create or install a new theme as your themes directory is not currently writable!';
-$txt['theme_installed'] = 'Installed Successfully';
-$txt['theme_installed_message'] = 'was installed successfully.';
+$txt['theme_install'] = 'Yeni Tema Yükle';
+$txt['theme_install_file'] = 'Kendi bilgisayarınızdan';
+$txt['theme_install_dir'] = 'Sunucudaki bir dizinden';
+$txt['theme_install_error'] = 'Belirtmiş olduğunuz dizin yanlış veya geçerli bir tema bulundurmamakta!';
+$txt['theme_install_write_error'] = 'Devam edilebilmesi için tema dizini yazılabilir olmalıdır!';
+$txt['theme_install_go'] = 'Yükle!';
+$txt['theme_install_new'] = 'Varsayılan temanın kopyasını oluştur';
+$txt['theme_install_new_confirm'] = 'Bu temayı yüklemek istediğinize emin misiniz?';
+$txt['theme_install_writable'] = 'Dikkat - tema dizininiz yazılabilir olmadığı için, yeni bir tema oluşturamaz veya yükleyemezsiniz!';
+$txt['theme_installed'] = 'Yükleme Tamamlandı';
+$txt['theme_installed_message'] = 'başarıyla yüklenmiştir.';
 
-$txt['theme_latest'] = 'Latest and Greatest Themes';
-$txt['theme_latest_fetch'] = 'Fetching latest themes from wedge.org...';
+$txt['theme_latest'] = 'En Yeni ve En Güzel Temalar';
+$txt['theme_latest_fetch'] = 'Wedge sunucusundan son temaların listesi yükleniyor...';
 
 $txt['theme_set'] = 'Use the default style';
 $txt['skin_users'] = array(
@@ -34,52 +34,52 @@ $txt['skin_users'] = array(
 	1 => '1 person is using this skin.',
 	'n' => '%s people are using this skin.',
 );
-$txt['current_theme'] = 'Current theme';
+$txt['current_theme'] = 'Mevcut tema';
 
-$txt['theme_skins'] = 'Skins';
-$txt['theme_skin_preview'] = 'Preview skin';
-$txt['theme_skin_set'] = 'Use this skin';
+$txt['theme_skins'] = 'Skinler';
+$txt['theme_skin_preview'] = 'Önizleme';
+$txt['theme_skin_set'] = 'Kullan';
 
-$txt['theme_edit'] = 'Edit Theme';
-$txt['theme_edit_style'] = 'Modify the stylesheets. (Colors, fonts, etc.)';
-$txt['theme_edit_index'] = 'Modify the index template. (The main template)';
-$txt['theme_edit_no_save'] = 'This file cannot be saved because it is not writable! Please make sure the following file is 777 or has the proper permissions';
-$txt['theme_edit_save'] = 'Save Changes';
+$txt['theme_edit'] = 'Temayı düzenle';
+$txt['theme_edit_style'] = 'Stil dosyasını düzenle. (renkler, yazı tipleri, vs.)';
+$txt['theme_edit_index'] = 'Index şablonunu düzenle. (ana şablon)';
+$txt['theme_edit_no_save'] = 'Bu dosya yazılabilir olmadığı için kaydedilemiyor!  Lütfen aşağıdaki dosyanın chmod 777 olduğundan veya uygun izinlerin verildiğinden emin olun';
+$txt['theme_edit_save'] = 'Değişiklikleri kaydet.';
 
-$txt['theme_global_description'] = 'This is the default theme, which means your theme will change along with the administrators\' settings and the board you are viewing.';
+$txt['theme_global_description'] = 'Bu tema varsayılan forum temasıdır. Forumda yapılacaklar değişiklikler bu tema üzerinde olacaktır.';
 
-$txt['themeadmin_title'] = 'Themes and Layout Settings';
-$txt['themeadmin_description'] = 'Here you can modify the settings for your themes, update theme selections, reset member options, and the like.';
-$txt['themeadmin_admin_desc'] = 'This page allows you to change the default theme, reset members to all use a certain theme, and choose other settings related to theme selection. You are also able to install themes from here.<br><br>Don\'t forget to look at the theme settings for your themes for layout options.';
-$txt['themeadmin_list_desc'] = 'From here, you can view the list of themes you currently have installed, change their paths and settings, and uninstall them.';
-$txt['themeadmin_edit_desc'] = 'Modify the stylesheet and source code of your installed themes. Please consult the documentation for more information.';
+$txt['themeadmin_title'] = 'Tema ve Görünüm Ayarları';
+$txt['themeadmin_description'] = 'Burada tema ayarlarınızı değiştirebilir, seçilebilir temaları güncelleyebilir ve kişisel birçok tema seçeneğini düzenleyebilirsiniz.';
+$txt['themeadmin_admin_desc'] = 'Burada varsayılan temayı değiştirebilir, tüm üyelerin temalarını sıfırlayabilir veya tema seçimi ile ilgili diğer ayarlarda değişiklikler yapabilirsiniz. Aynı zamanda buradan yeni temalar da yükleyebilirsiniz.<br /><br />Temanızın ayarlarını ve görünümünü değiştirmek için &quot;Temalar Ayarları&quot; na göz atmayı unutmayın.';
+$txt['themeadmin_list_desc'] = 'Burada yüklü olan temaları görebilir, konumlarını ve ayarlarını değiştirebilir veya kaldırabilirsiniz.';
+$txt['themeadmin_edit_desc'] = 'Burada yüklü temalarınızın css\'lerini ve kaynak kodlarını düzenleyebilirsiniz.  Lütfen daha fazla bilgi için dokümantasyona göz atın.';
 
-$txt['themeadmin_list_heading'] = 'Theme Settings Overview';
-$txt['themeadmin_list_tip'] = 'Remember, the layout settings may be different between one theme and another. Click on the theme\'s names below to set their options, change their directory or URL settings, or to find other options.';
-$txt['themeadmin_list_theme_dir'] = 'Theme directory (templates)';
-$txt['themeadmin_list_invalid'] = '(warning, this path is not correct!)';
-$txt['themeadmin_list_theme_url'] = 'URL to above directory';
-$txt['themeadmin_list_reset'] = 'Reset Theme URLs and Directories';
-$txt['themeadmin_list_reset_dir'] = 'Base path to Themes directory';
-$txt['themeadmin_list_reset_url'] = 'Base URL to the same directory';
-$txt['themeadmin_list_reset_go'] = 'Attempt to reset all themes';
+$txt['themeadmin_list_heading'] = 'Tema Ayarları';
+$txt['themeadmin_list_tip'] = 'Unutmayın görünüm ayarları bir temadan diğerine değişebilir.  Temaların özelliklerini ayarlamak için ayarlamak istediğiniz temanın üstüne tıklayın. Buradan ayrıca tema klasörlerinin konumlarını (URL\'lerini) değiştirebilirsiniz.';
+$txt['themeadmin_list_theme_dir'] = 'Temanın klasörü';
+$txt['themeadmin_list_invalid'] = '(girdiğiniz bilgi doğru değildir!)';
+$txt['themeadmin_list_theme_url'] = 'Yukarıdaki klasörün URL\'si';
+$txt['themeadmin_list_reset'] = 'Tüm tema URL\'lerini ve klasörlerini sıfırla';
+$txt['themeadmin_list_reset_dir'] = 'Ana tema klasörü';
+$txt['themeadmin_list_reset_url'] = 'Yukarıdaki klasörün URL\'si';
+$txt['themeadmin_list_reset_go'] = 'Tüm temaları sıfırla';
 
-$txt['themeadmin_edit_browse'] = 'Browse the templates and files in this theme.';
-$txt['themeadmin_edit_style'] = 'Browse this theme\'s stylesheets.';
-$txt['themeadmin_edit_copy_template'] = 'Copy a template from the theme this is based on.';
-$txt['themeadmin_edit_exists'] = 'already exists';
-$txt['themeadmin_edit_do_copy'] = 'copy';
-$txt['themeadmin_edit_copy_warning'] = 'When Wedge needs a template or language file which is not in the current theme, it looks in the theme it is based on, or the default theme.<br>Unless you need to modify a template, it\'s better not to copy it.';
-$txt['themeadmin_edit_copy_confirm'] = 'Are you sure you want to copy this template?';
-$txt['themeadmin_edit_overwrite_confirm'] = 'Are you sure you want to copy this template over the one that already exists?<br>This will OVERWRITE any changes you\'ve made!';
-$txt['themeadmin_edit_no_copy'] = '<em>(can\'t copy)</em>';
-$txt['themeadmin_edit_filename'] = 'Filename';
-$txt['themeadmin_edit_modified'] = 'Last Modified';
-$txt['themeadmin_edit_size'] = 'Size';
+$txt['themeadmin_edit_browse'] = 'Bu temadaki dosyalara göz atın ve düzenle.';
+$txt['themeadmin_edit_style'] = 'Bu temanın stil (css) dosyasını düzenle.';
+$txt['themeadmin_edit_copy_template'] = 'Bu tema tabanlı yeni bir tema oluştur.';
+$txt['themeadmin_edit_exists'] = 'zaten bulunmakta';
+$txt['themeadmin_edit_do_copy'] = 'kopyala';
+$txt['themeadmin_edit_copy_warning'] = 'Wedge ihtiyacı olduğu bir görünüm veya dil dosyasını ilgili temanın klasöründe bulamadığında, temanın tabanlı olduğu temada veya varsayılan tema da o dosyaları arar.<br />Eğer bir görünüm dosyasını değiştirmek istemiyorsanız her zaman onu kopyalamamak daha iyi bir fikirdir.';
+$txt['themeadmin_edit_copy_confirm'] = 'Bu temayı kopyalamak istediğinize emin misiniz?';
+$txt['themeadmin_edit_overwrite_confirm'] = 'Bu temayı diğerinin üzerine kopyalamak istediğinize emin misiniz?\\nBu yapmış olduğunuz tüm değişikliklerin KAYBOLMASINA neden olacaktır!';
+$txt['themeadmin_edit_no_copy'] = '<em>(kopyalanamıyor)</em>';
+$txt['themeadmin_edit_filename'] = 'Dosya Adı';
+$txt['themeadmin_edit_modified'] = 'Son Değiştirilme Tarihi';
+$txt['themeadmin_edit_size'] = 'Boyut';
 $txt['themeadmin_edit_bytes'] = 'B';
 $txt['themeadmin_edit_kilobytes'] = 'KB';
-$txt['themeadmin_edit_error'] = 'The file you tried to save generated the following error:';
-$txt['themeadmin_edit_on_line'] = 'Beginning on line';
-$txt['themeadmin_edit_preview'] = 'Preview';
-$txt['themeadmin_selectable'] = 'Themes that the user is able to select';
-$txt['themeadmin_themelist_link'] = 'Show the list of themes';
+$txt['themeadmin_edit_error'] = 'Kaydetmiş olduğunuz dosya aşağıdaki hataya neden oldu:';
+$txt['themeadmin_edit_on_line'] = 'Satır';
+$txt['themeadmin_edit_preview'] = 'Önizleme';
+$txt['themeadmin_selectable'] = 'Kullanılabilir temalar';
+$txt['themeadmin_themelist_link'] = 'Temaların listesini göster';
