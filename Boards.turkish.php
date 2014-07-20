b135b4492f3cc1b04f31d0a6a1ab09846cd9ee88
gevv <320voltmail@gmail.com>
2014-07-20 15:04:24 +0300

    Update Boards.turkish.php

--- Boards.turkish.php
+++ Boards.turkish.php
@@ -1,35 +1,35 @@
 <?php
 // Version: 2.0; Boards
 
-$txt['view_unread_category'] = 'Unread Posts';
+$txt['view_unread_category'] = 'Okunmamış İletiler';
 
 // And now for the moderation 'boards'
-$txt['moderation_cat'] = 'Moderation';
-$txt['reported_posts'] = 'Reported Posts';
-$txt['reported_board_desc'] = '<strong>Reported</strong> by {author}<br>in {link}<br>{time}';
+$txt['moderation_cat'] = 'Moderasyon';
+$txt['reported_posts'] = 'Rapor Edilen İletiler';
+$txt['reported_board_desc'] = '<strong>Rapor Eden:</strong> {author}<br>in {link}<br>{time}';
 $txt['reported_open'] = array(
-	1 => '1 open report',
-	'n' => '%1$s open reports',
+	1 => '1 açık rapor',
+	'n' => '%1$s açık rapor',
 );
 $txt['reported_closed'] = array(
-	1 => '1 closed report',
-	'n' => '%1$s closed reports',
+	1 => '1 kapalı rapor',
+	'n' => '%1$s kapalı rapor',
 );
-$txt['group_requests'] = 'Group Requests';
+$txt['group_requests'] = 'Grup İstekleri';
 $txt['group_request_desc'] = 'Joining <strong>{groupname}</strong><br>by {author}<br>{time}';
 $txt['group_req_open'] = array(
-	1 => '1 open request',
-	'n' => '%1$s open requests',
+	1 => '1 açık istek',
+	'n' => '%1$s açık istek',
 );
 $txt['group_req_closed'] = array(
-	1 => '1 closed request',
-	'n' => '%1$s closed requests',
+	1 => '1 kapalı istek',
+	'n' => '%1$s kapalı istek',
 );
-$txt['logs_board'] = 'Logs';
-$txt['logs_moderation'] = 'Moderation Log';
-$txt['logs_infractions'] = 'Infraction Log';
+$txt['logs_board'] = 'Kayıtlar';
+$txt['logs_moderation'] = 'Moderasyon Kayıtları';
+$txt['logs_infractions'] = 'Yasaklama Kayıtları';
 $txt['error_log_count'] = array(
-	0 => 'No errors',
-	1 => '1 error',
-	'n' => '%1$s errors',
+	0 => 'Hata Yok',
+	1 => '1 hata',
+	'n' => '%1$s hata',
 );
